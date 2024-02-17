29917077dd785f6945589de4c274748e7aa4105e
ashifmahmudsiam <138794099+ashifmahmudsiam@users.noreply.github.com>
2024-02-17 14:19:40 +0600

    update v2

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
-let fit = document.querySelector(".fit");
-let inch = document.querySelector(".inch");
+let fit = document.querySelector(".fit"); let inch = document.querySelector(".inch");
 
 fit.addEventListener("input", (e) => {
-  let i = fit.value * 12;
-  if (isNaN(i)) {
-    alert("please input a number");
-    fit.value = "";
-  } else {
-    inch.value = i;
-  }
+
+let i fit.value * 12;
+
+if (isNaN(i)) {
+
+alert("please input a number");
+
+fit.value = "";
+
+} else {
+
+inch.value = i;
+
+}
+
 });
+
 inch.addEventListener("input", (e) => {
-  let i = inch.value / 12;
-  if (isNaN(i)) {
-    alert("please input a number");
-    inch.value = "";
-  } else if (!Number.isInteger(i)) {
-    let h = (fit.value = i.toFixed(2));
-    fit.value = h;
-  }
+
+let i inch.value / 12;
+
+if (isNaN(i)) {
+
+alert("please input a number");
+
+inch.value = "";
+
+} else if (Number.isInteger(i)) {
+
+let hi.toFixed(2);
+
+fit.value = h;
+
+}
+
 });
